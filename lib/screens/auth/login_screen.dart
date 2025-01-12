95be2531d7e5cf45802319f2b1d4232392ef48a0
fwdAndroid <fwdkaleem@gmail.com>
2025-01-12 18:28:39 +0500

    OTP Implemented

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:muslim_mariage/screens/profile/complete_profile.dart';
+import 'package:muslim_mariage/screens/auth/otp_screen.dart';
 import 'package:muslim_mariage/utils/colors.dart';
-import 'package:intl_phone_field/intl_phone_field.dart';
+import 'package:form_field_validator/form_field_validator.dart';
+
+import 'package:country_code_picker/country_code_picker.dart';
 import 'package:muslim_mariage/widgets/save_button.dart';
-import 'package:flutter_social_button/flutter_social_button.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -13,6 +14,9 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController _controller = TextEditingController();
+
+  String dialCodeDigits = "+91";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,35 +44,52 @@ class _LoginScreenState extends State<LoginScreen> {
           ),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: IntlPhoneField(
-              decoration: InputDecoration(
-                hintText: 'Enter phone number',
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(),
-                ),
+            child: Container(
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.blueAccent),
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white70),
+              margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+              child: Row(
+                children: [
+                  CountryCodePicker(
+                      onChanged: (country) {
+                        setState(() {
+                          dialCodeDigits = country.dialCode!;
+                        });
+                      },
+                      initialSelection: "IN",
+                      showCountryOnly: false,
+                      showOnlyCountryWhenClosed: false,
+                      favorite: const ["+91", "IN"]),
+                  Expanded(
+                    child: Container(
+                      child: TextFormField(
+                        validator: RequiredValidator(errorText: "Required"),
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: "12345678",
+                          //  prefix: Padding(padding: EdgeInsets.all(10),child: Text(dialCodeDigits,style: TextStyle(color: Colors.black),),),
+                        ),
+                        keyboardType: TextInputType.number,
+                        controller: _controller,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-              initialCountryCode: 'IN', // Default country code
-              onChanged: (phone) {
-                print(phone
-                    .completeNumber); // Prints the complete phone number with country code
-              },
             ),
           ),
           SaveButton(
               title: "Continue",
               onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (builder) => CompleteProfile()));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (builder) => VerifyPhone(
+                            codeDigits: dialCodeDigits,
+                            phone: _controller.text)));
               }),
-          FlutterSocialButton(
-            title: "Login with Google",
-            titleStyle: TextStyle(color: colorWhite),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (builder) => CompleteProfile()));
-            },
-            buttonType: ButtonType.google,
-          )
         ],
       ),
     );
