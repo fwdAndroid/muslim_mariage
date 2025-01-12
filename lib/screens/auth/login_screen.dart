884f8850bbaf5d6c8662f00642aa4ffd20e34749
fwdAndroid <fwdkaleem@gmail.com>
2025-01-12 18:50:52 +0500

    Google Sign IN Process Complete

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,7 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:muslim_mariage/screens/main/main_dashboard.dart';
 import 'package:muslim_mariage/screens/profile/complete_profile.dart';
+import 'package:muslim_mariage/services/auth_methods.dart';
 import 'package:muslim_mariage/utils/colors.dart';
-import 'package:muslim_mariage/widgets/save_button.dart';
+import 'package:muslim_mariage/utils/showmesssage.dart';
 import 'package:flutter_social_button/flutter_social_button.dart';
 
 class LoginScreen extends StatefulWidget {
@@ -12,10 +16,12 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool isGoogle = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Padding(
@@ -37,21 +43,85 @@ class _LoginScreenState extends State<LoginScreen> {
               textAlign: TextAlign.center,
             ),
           ),
-          SaveButton(
-              title: "Continue",
-              onTap: () {
-                Navigator.push(context,
-                    MaterialPageRoute(builder: (builder) => CompleteProfile()));
-              }),
-          FlutterSocialButton(
-            title: "Login with Google",
-            titleStyle: TextStyle(color: colorWhite),
-            onTap: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (builder) => CompleteProfile()));
-            },
-            buttonType: ButtonType.google,
-          )
+          SizedBox(height: 20),
+          isGoogle
+              ? Center(child: CircularProgressIndicator())
+              : Center(
+                  child: FlutterSocialButton(
+                      buttonType: ButtonType.google,
+                      onTap: () async {
+                        setState(() {
+                          isGoogle = true;
+                        });
+
+                        await AuthMethods().signInWithGoogle();
+
+                        try {
+                          // Get the UserCredential object
+
+                          final userDoc = await FirebaseFirestore.instance
+                              .collection("users")
+                              .doc(FirebaseAuth.instance.currentUser!.uid)
+                              .get();
+
+                          if (userDoc.exists) {
+                            // Retrieve user data
+                            final userData = userDoc.data();
+                            final fullName = userData?['fullName'];
+                            final phone = userData?['phone'];
+                            final location = userData?['location'];
+
+                            // Check if any field is null or empty
+                            if ((fullName == null || fullName.isEmpty) ||
+                                (phone == null || phone.isEmpty) ||
+                                (location == null || location.isEmpty)) {
+                              // Navigate to ProfilePage1 to complete the profile
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (builder) => CompleteProfile()),
+                              );
+                            } else {
+                              // All fields are valid, navigate to MainDashboard
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (builder) => MainDashboard()),
+                              );
+                            }
+                          } else {
+                            // If the user does not exist, create a new document
+                            await FirebaseFirestore.instance
+                                .collection("users")
+                                .doc(FirebaseAuth.instance.currentUser!.uid)
+                                .set({
+                              "image":
+                                  FirebaseAuth.instance.currentUser!.photoURL,
+                              "email": FirebaseAuth.instance.currentUser!.email,
+                              "uid": FirebaseAuth.instance.currentUser!.uid,
+                              "fullName": "",
+                              "phone": "",
+                              "location": "",
+                            });
+
+                            // Navigate to ProfilePage1 for new users
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (builder) => CompleteProfile()),
+                            );
+                          }
+                        } catch (e) {
+                          showMessageBar(
+                              "Error during sign-in. Please try again. $e",
+                              context);
+                        }
+
+                        setState(() {
+                          isGoogle = false;
+                        });
+                      }),
+                )
         ],
       ),
     );
