288797bcb97719adb12ed57bfc2419de3b047703
fwdAndroid <fwdkaleem@gmail.com>
2025-01-12 19:23:58 +0500

    Profile Completed

--- lib/screens/profile/complete_profile.dart
+++ lib/screens/profile/complete_profile.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart'; // Import Firestore
 import 'package:muslim_mariage/screens/profile/upload_photo.dart';
 import 'package:muslim_mariage/utils/colors.dart';
+import 'package:muslim_mariage/utils/showmesssage.dart';
 import 'package:muslim_mariage/widgets/save_button.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart'; // Import SpinKit for stylish loader
 
 class CompleteProfile extends StatefulWidget {
-  const CompleteProfile({super.key});
+  const CompleteProfile({Key? key}) : super(key: key);
 
   @override
   State<CompleteProfile> createState() => _CompleteProfileState();
 }
 
 class _CompleteProfileState extends State<CompleteProfile> {
+  String _selectedSect = 'Sunni'; // Default value
+  String _selectedGender = 'Male'; // Default value
+  String _selectedMaritalStatus = 'Single'; // Default value
+  String _profileCreator = 'Self';
+
+  final TextEditingController _fatherController = TextEditingController();
+  final TextEditingController _motherController = TextEditingController();
+  final TextEditingController _dobController = TextEditingController();
+  final TextEditingController _contactNumberController =
+      TextEditingController();
+  final TextEditingController _qualificationController =
+      TextEditingController();
+  final TextEditingController _jobOccupationController =
+      TextEditingController();
+  final TextEditingController _yourselfController = TextEditingController();
+  final TextEditingController _userNameController = TextEditingController();
+  bool _isLoading = false; // Track loading state
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,32 +62,51 @@ class _CompleteProfileState extends State<CompleteProfile> {
           padding: const EdgeInsets.all(8),
           child: Column(
             children: [
-              _buildTextField('Father Name', Icons.person),
+              _buildDropdownField('Profile Creator',
+                  ['Self', 'Father', 'Mother', 'Brother', "Uncle"],
+                  initialValue: _profileCreator),
               SizedBox(height: 12),
-              _buildTextField('Mother Name', Icons.person),
+              _buildTextField('Father Name', _fatherController, Icons.person),
               SizedBox(height: 12),
-              _buildTextField('Date of Birth', Icons.calendar_today),
+              _buildTextField('Mother Name', _motherController, Icons.person),
               SizedBox(height: 12),
-              _buildTextField('Contact Number', Icons.phone),
+              _buildTextField('Full Name', _userNameController, Icons.person),
               SizedBox(height: 12),
-              _buildDropdownField('Your Sect'),
+              _buildTextField(
+                  'Date of Birth', _dobController, Icons.calendar_today),
               SizedBox(height: 12),
-              _buildDropdownField('Gender'),
+              _buildTextField(
+                  'Contact Number', _contactNumberController, Icons.phone),
               SizedBox(height: 12),
-              _buildDropdownField('Matriarchal Status'),
+              _buildDropdownField('Your Sect', ['Sunni', 'Shia'],
+                  initialValue: _selectedSect),
               SizedBox(height: 12),
-              _buildTextField('Qualification', null),
+              _buildDropdownField('Gender', ['Male', 'Female'],
+                  initialValue: _selectedGender),
               SizedBox(height: 12),
-              _buildTextField('Job Occupation', Icons.work),
+              _buildDropdownField('Marital Status',
+                  ['Single', 'Married', 'Divorced', 'Widowed'],
+                  initialValue: _selectedMaritalStatus),
               SizedBox(height: 12),
-              _buildTextField('About Yourself', null, maxLines: 4),
+              _buildTextField('Qualification', _qualificationController, null),
+              SizedBox(height: 12),
+              _buildTextField(
+                  'Job Occupation', _jobOccupationController, Icons.work),
+              SizedBox(height: 12),
+              _buildTextField('About Yourself', _yourselfController, null,
+                  maxLines: 4),
               SizedBox(height: 20),
               SaveButton(
-                  title: "Continue",
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (builder) => UploadPhoto()));
-                  }),
+                title: "Continue",
+                onTap: _saveProfile,
+              ),
+              if (_isLoading)
+                Center(
+                  child: SpinKitCircle(
+                    color: mainColor,
+                    size: 50.0,
+                  ),
+                ),
             ],
           ),
         ),
@@ -75,8 +114,63 @@ class _CompleteProfileState extends State<CompleteProfile> {
     );
   }
 
-  Widget _buildTextField(String labelText, IconData? icon, {int maxLines = 1}) {
+  void _saveProfile() {
+    if (_fatherController.text.isEmpty ||
+        _motherController.text.isEmpty ||
+        _dobController.text.isEmpty ||
+        _contactNumberController.text.isEmpty ||
+        _qualificationController.text.isEmpty ||
+        _jobOccupationController.text.isEmpty ||
+        _yourselfController.text.isEmpty) {
+      _showAlert('Please fill all fields before continuing.');
+      return;
+    }
+
+    FirebaseFirestore.instance.collection('profiles').add({
+      'profileCreator': _profileCreator,
+      'fatherName': _fatherController.text,
+      'motherName': _motherController.text,
+      'fullName': _userNameController.text,
+      'dob': _dobController.text,
+      'contactNumber': _contactNumberController.text,
+      'sect': _selectedSect,
+      'gender': _selectedGender,
+      'maritalStatus': _selectedMaritalStatus,
+      'qualification': _qualificationController.text,
+      'jobOccupation': _jobOccupationController.text,
+      'aboutYourself': _yourselfController.text,
+    }).then((_) {
+      showMessageBar("Profile Created Successfully", context);
+      Navigator.push(
+          context, MaterialPageRoute(builder: (builder) => UploadPhoto()));
+    }).catchError((error) {
+      _showAlert('Failed to save profile: $error');
+    });
+  }
+
+  void _showAlert(String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Error'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTextField(
+      String labelText, TextEditingController controller, IconData? icon,
+      {int maxLines = 1}) {
     return TextField(
+      controller: controller,
       maxLines: maxLines,
       decoration: InputDecoration(
         labelText: labelText,
@@ -93,8 +187,10 @@ class _CompleteProfileState extends State<CompleteProfile> {
     );
   }
 
-  Widget _buildDropdownField(String labelText) {
+  Widget _buildDropdownField(String labelText, List<String> items,
+      {required String initialValue}) {
     return DropdownButtonFormField<String>(
+      value: initialValue,
       decoration: InputDecoration(
         labelText: labelText,
         border: OutlineInputBorder(
@@ -106,13 +202,25 @@ class _CompleteProfileState extends State<CompleteProfile> {
           borderSide: BorderSide(color: Colors.red),
         ),
       ),
-      items: ['Option 1', 'Option 2', 'Option 3'].map((String value) {
+      items: items.map((String value) {
         return DropdownMenuItem<String>(
           value: value,
           child: Text(value),
         );
       }).toList(),
-      onChanged: (_) {},
+      onChanged: (String? newValue) {
+        setState(() {
+          if (labelText == "Profile Creator") {
+            _profileCreator = newValue!;
+          } else if (labelText == 'Your Sect') {
+            _selectedSect = newValue!;
+          } else if (labelText == 'Gender') {
+            _selectedGender = newValue!;
+          } else if (labelText == 'Marital Status') {
+            _selectedMaritalStatus = newValue!;
+          }
+        });
+      },
     );
   }
 }
